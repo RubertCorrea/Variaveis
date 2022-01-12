6dc301b725c8eddafbfb1bcb88f731de963562fc
João Vitor <joao.rubertcorrea@gmail.com>
2022-01-12 15:54:43 -0300

    Var e let

---
+++ index.js
@@ -0,0 +1,9 @@
+var firstName = "João";
+var lastName = "Souza";
+
+if (firstName === "João"){
+    var firstName = "pedro"; // variavel global
+    let lastName = "silva"; // variavel em bloco 
+}
+
+console.log(firstName, lastName);
